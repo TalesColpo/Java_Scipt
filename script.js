bac3af06f9e4f90260b5faeb10f23ef0490b9ddc
TalesColpo <101259550+TalesColpo@users.noreply.github.com>
2022-11-20 10:08:06 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+let name = "Tales"; 
+let age = "21"
+
+let ultraage = age * 7;
+console.log(ultraage);
+
+let friends = ["Ali", "Fri", "Raf"];
+
+let person1 = {
+    name: "Ali",
+    age: 21,
+    isStudent: true
+  };
+
+  let person2 = {
+    name: "Fri",
+    age: 25,
+    isStudent: true
+  };
+
+  let person3 = {
+    name: "Raf",
+    age: 40,
+    isStudent: true
+  };
+
+let IFRIENDS = [
+    {
+      name: "Ali",
+      age: 21,
+      isStudent: true
+    },
+    {
+      name: "Fri",
+      age: 25,
+      isStudent: true
+    },
+    {
+      name: "Raf",
+      age: 40,
+      isStudent: true
+    }
+  ];
+  console.log(IFRIENDS[0].name);
+  // João
